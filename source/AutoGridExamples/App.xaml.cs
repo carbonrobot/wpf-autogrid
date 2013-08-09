5013349cd2d9032add983a8b9de13c9aaae3f602
Charles Brown <carbonrobot@gmail.com>
2013-08-09 12:18:22 -0500

    Added source project

---
+++ source/AutoGridExamples/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AutoGridExamples
+{
+    public partial class App : Application
+    {
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    base.OnStartup(e);
+        //    if (true)
+        //    {
+        //        this.Shutdown();
+        //    }
+        //}
+    }
+}
---
+++ source/WpfAutoGrid/DependencyExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace WpfAutoGrid
+{
+    using System.Windows;
+
+    public static class DependencyExtensions
+    {
+        /// <summary>
+        /// Sets the value of the <paramref name="property"/> only if it hasn't been explicitly set.
+        /// </summary>
+        public static bool SetIfDefault<T>(this DependencyObject o, DependencyProperty property, T value)
+        {
+            if (DependencyPropertyHelper.GetValueSource(o, property).BaseValueSource == BaseValueSource.Default)
+            {
+                o.SetValue(property, value);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
